116889de254cbb6a36f39c212c8ce6ce0a666bde
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2025-10-30 20:09:03 +0100

    Primer commit del repositorio AHFDWESProyectoDWES

---
+++ indexProyectoDWES.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../webroot/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" type="image/png" href="/Proyecto webs alumnos/media/logoSauces.png">
+    <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Double:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <title>Alejandro De la Huerga</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <img src="../webroot/images/logoSauces.png" alt="logoIesSauces" width="100" height="100">
+            <h1><a href="../index.html">Alejandro De la Huerga </a></h1>
+        </div>
+        
+    </header>
+    <main>
+        <section id="asignaturas">
+            <h2>DESARROLLO WEB ENTORNO SERVIDOR</h2>
+            <ul>
+                <li><a href="doc/EstudioTema1.pdf" target="_blank">Tema 1: DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+                <li><a href="doc/EstudioTema2.html" target="_blank">Tema 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+                <li><a href="../AHFDWESProyectoTema3/indexProyectoTema3.php">Tema 3: CARACTERISTICAS DEL LENGUAJE PHP</a></li>
+                <li><p>Tema 4: TÉCNICAS DE ACCESO A DATOS EN PHP </p></li>
+                <li><p>Tema 5: DESARROLLO DE APLICACIONES WEB</p></li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p><a href="../index.html">&copy; 2024 Alejandro De la Huerga. Todos los derechos reservados.</a></p>
+    </footer>
+</body>
+</html>
