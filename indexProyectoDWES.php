66f89d3a3fadc690ff2ec13890a4e7a5b7b38e31
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2025-12-16 21:36:28 +0100

    MODIFICACION INDEX

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -112,7 +112,7 @@
                 <li>TEMA 7: PROGRAMACIÓN DE SERVICIOS WEB</li>
                 <li id="tema5">
                     <p>TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS </p>
-                    <p>LOGIN LOGOFF</p>
+                    <a href="../AHFDWESLoginLogoff/indexLoginLogoff.php">LOGIN LOGOFF</a>
                     <p>APLICACIÓN FINAL</p>
                     <p>APLICACIÓN WORDPRESS</p>
                 </li>
